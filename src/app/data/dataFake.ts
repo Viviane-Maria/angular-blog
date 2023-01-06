871ac2f020f187fd7319ead7ff47340ad0551b5a
Viviane-Maria <viviane.m.oliveira.s@gmail.com>
2023-01-06 17:04:51 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,86 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title": "WANDINHA",
+    "description":"Série ultrapassa Stranger Things em visualizações",
+    "photoCover":"https://pbs.twimg.com/media/FlA8742XEAElTkG?format=jpg&name=large",
+    "contentNews":"Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus, neque quisquam. Tenetur quam, neque commodi officia velit amet quo veniam dolore placeat sed ut voluptatem molestias inventore at! Reprehenderit, dolor? Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minima eum molestias natus iste recusandae deserunt maiores consequuntur pariatur? Quibusdam debitis omnis similique iusto, doloribus sapiente tempore deserunt rem nisi veritatis!"
+  },
+  {
+    "id":"1.1",
+    "title": "Lista de séries coreanas para você assistir",
+    "description":"",
+    "photoCover":"https://observatoriodocinema.uol.com.br/wp-content/uploads/2022/08/Extraordinary-Attorney-Woo.jpg",
+    "contentNews":"Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo at ab quaerat adipisci commodi ratione qui aspernatur, eveniet ipsum expedita saepe cum sed iste ipsam doloribus cumque asperiores! Quasi, mollitia. Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, accusantium a voluptas vitae assumenda sunt tenetur sint aliquam sit! Necessitatibus eligendi expedita quae blanditiis quod illum ratione maxime praesentium perferendis!"
+  },
+  {
+    "id":"1.2",
+    "title": "Novidades no elenco da série Os Anéis do Poder",
+    "description":"",
+    "photoCover":"https://technewsbrasil.com.br/sobresagas/wp-content/uploads/2022/02/os-aneis-de-poder-1.jpg",
+    "contentNews":"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem, molestias optio quos similique qui quibusdam rem consequatur dolorem excepturi vero voluptates, laborum, illo beatae cumque consectetur assumenda. Accusantium, ratione dolor? Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus eos perspiciatis possimus laborum ab, laudantium dolor ea adipisci veniam itaque quisquam architecto. Quam nam doloremque quia ratione necessitatibus distinctio amet."
+  },
+  {
+    "id":"1.3",
+    "title": "3ª Temporada 'The Mandalorian",
+    "description":"",
+    "photoCover":"https://i.ibb.co/YhZXMM4/mandalorian-lede-1300x731-1.jpg",
+    "contentNews":"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo officia mollitia, sequi accusantium libero, id ducimus ratione, placeat provident aperiam veniam praesentium repellendus non deleniti ipsam iusto asperiores aliquam perferendis. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi quod mollitia pariatur vitae quaerat. Optio, dolor amet ea quis error quibusdam nulla? Tempora numquam labore, earum debitis placeat accusantium fugit."
+  },
+  {
+    "id":"2",
+    "title": "AVATAR: O Caminho da Água",
+    "description":"Filme pode superar a arrecadação total de 'Top Gun: Maverick'",
+    "photoCover":"https://www.showmetech.com.br/wp-content/uploads//2022/12/Avatar-1.jpg",
+    "contentNews":"Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita, adipisci quasi facere ipsum fugiat quisquam assumenda pariatur odio. Necessitatibus explicabo recusandae doloribus repellat similique provident vero ipsa sed rem dolore. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Modi, fugit? Laudantium pariatur sapiente fugit consequuntur consequatur in, facere iste vel quasi, eius, iure cumque nemo dolores deleniti beatae sequi repudiandae!"
+  },
+  {
+    "id":"2.1",
+    "title": "MATILDA: O Musical - Disponível na Netflix",
+    "description":"",
+    "photoCover":"https://cloud.estacaonerd.com/wp-content/uploads/2022/12/25122657/MATILDDA.jpg",
+    "contentNews":"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dignissimos, laboriosam molestiae. Quo reprehenderit, nam nostrum praesentium ipsum delectus ullam, animi ab sequi reiciendis quod sint libero quasi voluptatem cupiditate eos? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sapiente, facere architecto odit rem accusamus unde autem, a vitae ipsam dolorum fugit adipisci, at minima atque? In excepturi molestiae accusamus repellendus?"
+  },
+  {
+    "id":"2.2",
+    "title": "AQUAMAN e o Reino Perdido: pode ser a última vez de Jason Momoa no papel do herói",
+    "description":"",
+    "photoCover":"https://i0.wp.com/portrasdamagia.com.br/wp-content/uploads/2021/06/Aquaman-2-3.jpg?fit=960%2C500&ssl=1",
+    "contentNews":"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nesciunt unde illo obcaecati consequatur, quos explicabo eaque? Tenetur, porro ea tempora corporis reiciendis quis illo voluptatibus ad, suscipit, aliquid expedita doloribus? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Similique possimus aliquam maxime sint voluptatum autem error excepturi et nesciunt reprehenderit. Tenetur id cumque blanditiis maiores quasi a quis velit fugiat!"
+  },
+  {
+    "id":"2.3",
+    "title": "GUARDIÕES DA GALÁXIA: Vol. 3 - Lançamento para 2023",
+    "description":"",
+    "photoCover":"https://oimparcial.com.br/app/uploads/2022/12/Guardians-of-the-Galaxy-Vol.-3-min-1.jpg",
+    "contentNews":"Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio harum nulla cupiditate praesentium blanditiis, fuga odio eligendi labore est id molestiae quis enim ducimus quae in incidunt ratione minus sunt! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Excepturi quidem maiores sed, fugit dicta repellat dignissimos qui praesentium! Ut nam eum laborum. Neque tempora nam reiciendis autem sit vel quisquam."
+  },
+  {
+    "id":"3",
+    "title": "Animais Fantásticos e Onde Habitam",
+    "description":"Edição de 2001 com comentários de Harry, Rony e Hermione se torna rara",
+    "photoCover":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS24A7Fx_OYTDJSl88TI4JzmbKhe0Z13mjfDubt-A4OHcc5IWt2",
+    "contentNews":"Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse porro et libero officia quibusdam magni voluptas quas saepe nulla eaque quam, earum repellat molestias dolorem quaerat, asperiores accusantium voluptates ut! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nostrum repudiandae non impedit, rerum ratione id obcaecati aperiam hic et possimus provident iste adipisci vero esse deleniti, rem illo omnis error. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Et quisquam, a, dolores fugiat cupiditate illo non quis dolorem error sit velit quam quos deserunt alias quas reprehenderit, quaerat sint pariatur."
+  },
+  {
+    "id":"3.1",
+    "title": "Orgulho e Preconceito: o clássico de Jane Austen encanta a nova geração",
+    "description":"",
+    "photoCover":"https://4.bp.blogspot.com/-ZWPmRXmEpLg/WT3C2whh5kI/AAAAAAAAUuI/99T3Bo6GaoY0zspPG0OhQKqHquBFPQzIACLcB/s1600/orgulho%2B2.JPG",
+    "contentNews":"Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus quia corporis nemo temporibus facere earum beatae ipsa iusto quam quibusdam blanditiis laborum aliquam modi sunt, omnis eum neque doloremque vitae. Lorem ipsum, dolor sit amet consectetur adipisicing elit. At rerum beatae animi ipsam ut neque veniam modi dignissimos quaerat? Dolore eius at officiis inventore nulla labore omnis? Ut, dicta qui? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia cupiditate itaque voluptates harum error dolorum. Tempora esse minima repellat sequi odio, aperiam quisquam, quos aliquam voluptates laboriosam totam soluta nulla."
+  },
+  {
+    "id":"3.2",
+    "title": "23 livros clássicos que você deveria ler",
+    "description":"",
+    "photoCover":"https://www.taglivros.com/blog/wp-content/uploads/2022/03/BLOG_Capa-Por-que-ler-livros-cl%C3%A1ssicos.jpg",
+    "contentNews":"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsum repudiandae veritatis modi obcaecati accusamus eaque corrupti dolores vitae recusandae doloremque molestias, delectus dolore quibusdam ducimus inventore ratione eius omnis ea? Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis corrupti ut voluptatum assumenda eligendi quasi? Quia, magnam impedit atque veniam voluptates, consectetur obcaecati itaque rem distinctio velit quaerat minus odit? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam incidunt voluptatibus mollitia deleniti doloremque eum, quo consequuntur fuga officia iste aliquid minima velit sunt facere harum laboriosam. Atque, eos rerum."
+  },
+  {
+    "id":"3.3",
+    "title": "5 livros de mindset para você ler",
+    "description":"",
+    "photoCover":"hhttps://hss-consulting.com.br/wp-content/uploads/2018/04/O-Vendedor-de-Mindset-Fixo-1.png",
+    "contentNews":"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odio autem deleniti, blanditiis earum aut ducimus perferendis fugit molestias reprehenderit beatae modi! Recusandae consequuntur voluptas explicabo vel voluptatem? Laboriosam, amet nemo! Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio dolore deserunt vero a, facere, voluptatibus nobis quos delectus commodi aspernatur eveniet iste rem hic ut consequuntur architecto quia quas libero. Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero ducimus sed nesciunt dolores, praesentium voluptatum aliquam, excepturi fuga earum unde ad et inventore non culpa. Ea ex officia illo vero."
+  }
+]
